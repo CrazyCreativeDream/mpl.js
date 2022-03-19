c1e998fd839f1b7dc7cce72535fb3c33dfdea50e
ChenYFan <chen2778754364@foxmail.com>
2022-03-19 14:41:19 +0000

    T 尝试支持Heroku

---
+++ api/heroku.js
@@ -0,0 +1,53 @@
+import fetch, {
+    Blob,
+    blobFrom,
+    blobFromSync,
+    File,
+    fileFrom,
+    fileFromSync,
+    FormData,
+    Headers,
+    Request,
+    Response
+} from 'node-fetch'
+
+import handle from './../handle/index.js'
+import http from 'http'
+http.createServer(async (req, res) => {
+    const request = new Request(new URL(req.url, 'https://localhost'), {
+        method: req.method,
+        headers: new Headers(req.headers),
+        body: req.method !== 'GET' && req.method !== 'HEAD' ? await (new Promise((resolve, reject) => {
+            let data = []
+            req.on('data', chunk => {
+                data.push(chunk)
+            })
+            req.on('end', () => {
+                resolve(Buffer.concat(data))
+            })
+        })) : null
+    })
+    const response = await handle(request)
+    response.headers.forEach((value, key) => {
+        if(
+            key.toLowerCase() == 'connection' ||
+            key.toLowerCase() == 'content-length' ||
+            key.toLowerCase() == 'content-encoding' ||
+            key.toLowerCase() == 'transfer-encoding' ||
+            key.toLowerCase() == 'date' ||
+            key.toLowerCase() == 'server'
+        ){
+            return
+        }else{
+
+            res.setHeader(key, value)
+        }
+    })
+    res.writeHead(response.status || 200,response.statusText || "OK")
+    
+    res.end(Buffer.from(await response.arrayBuffer()))
+
+
+}).listen(process.env.PORT || 9000)
+
+
